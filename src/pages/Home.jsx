9874dbb49867009d84e56bee284f6737b9417ff4
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2026-02-08 14:11:55 +0100

    Adding footer to the home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ import { Navbar } from '../components/NavBar';
 import { HeroSection } from '../components/HeroSection';
 import { AboutSection } from '../components/AboutSection';
 import { SkillsSection } from '../components/SkillsSection';
-
+import { ProjectsSection } from '../components/ProjectsSection';
+import { ContactSection } from '../components/ContactSection';
+import { Footer } from '../components/Footer'
 
 export const Home = () => {
     return (
@@ -24,13 +26,13 @@ export const Home = () => {
         <main>
             <HeroSection />
             <AboutSection />
-            <SkillsSection />
-            {/* 
+            <SkillsSection />     
             <ProjectsSection />
-            <ContactSection /> */}
+            <ContactSection /> 
         </main>
 
         {/* Footer */}
+        <Footer/>
     </div>
     )
 };
