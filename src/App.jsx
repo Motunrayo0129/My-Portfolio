ae61281593165f979267d70e811ac0f6e0ee975f
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2026-02-08 14:00:38 +0100

    modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home.jsx';
 import { NotFound } from './pages/NotFound.jsx';
 
+
+
 function App() {
    return (
     <>
+    
     <Routes>
       <Route index element={<Home/>} />
       <Route path="*" element={<NotFound/>} />
